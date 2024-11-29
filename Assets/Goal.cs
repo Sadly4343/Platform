2382738abdbeda2c61f3452d7c2154f9e0e4e55d
Sadly4343 <loganhartshorn@gmail.com>
2024-11-29 04:45:32 -0700

    co

--- Assets/Goal.cs
+++ Assets/Goal.cs
@@ -6,16 +6,16 @@ using UnityEngine.SceneManagement;
 public class Goal : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private string Level_1;
+    [SerializeField] private string Level2;
 
     // Update is called once per frame
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("trigger caused");
+        Debug.Log("trigger caused" + other.gameObject.name);
         if (other.CompareTag("Player"))
         {
             Debug.Log("Player reached");
-            SceneManager.LoadScene(Level_1);
+            SceneManager.LoadScene(Level2);
         }
     }
 }
