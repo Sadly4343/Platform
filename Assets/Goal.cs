0969d6d9d0b617f9dfa3809704f812ed32534763
Sadly4343 <loganhartshorn@gmail.com>
2024-11-29 05:16:57 -0700

    co

--- Assets/Goal.cs
+++ Assets/Goal.cs
@@ -5,17 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour
 {
-    // Start is called before the first frame update
-    [SerializeField] private string Level2;
+    // The scene to load
+    private string Level2;
 
-    // Update is called once per frame
-    private void OnTriggerEnter(Collider other)
+
+    // This method is automatically called when something enters the trigger collider
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("trigger caused" + other.gameObject.name);
-        if (other.CompareTag("Player"))
+        // Log to the console when the trigger is hit
+
+        // Check if the object has the tag "Player"
+        if (collision.CompareTag("Player"))
         {
-            Debug.Log("Player reached");
-            SceneManager.LoadScene(Level2);
+            Debug.Log("Player reached goal!");
+            SceneManager.LoadScene(sceneBuildIndex: 1, LoadSceneMode.Single);
         }
     }
 }
